65d7d50759e9bcb5f133c66c232d0771406ae330
claudia v <claudia4ro@yahoo.com>
2020-05-22 01:08:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,11 +27,11 @@ class Triangle
       # end
       
     else
-      if length_1 == length_2 || length_2 == length_3 || length_1 == length_3
-        :isosceles
-        
-      elsif length_1 == length_2 && length_1 == length_3 && length_2 == length_3
+      if length_1 == length_2 && length_1 == length_3 
         :equilateral
+        
+      elsif  length_1 == length_2 || length_2 == length_3 || length_1 == length_3
+        :isosceles
         
       elsif length_1 != length_2 && length_2 != length_3 && length_1 != length_3
         :scalene

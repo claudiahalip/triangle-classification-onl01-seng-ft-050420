32551046a5fac28755a445c7fd53fdfbbcd83400
claudia v <claudia4ro@yahoo.com>
2020-05-22 01:21:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
       #     puts error.message 
       # end
       
-    elsif (length_1 + length_2) < length_3 || (length_2 + length_3) < length_1 || (length_1 + length_3) < length_2
+    elsif (length_1 + length_2) <= length_3 || (length_2 + length_3) <= length_1 || (length_1 + length_3) <= length_2
       raise TriangleError
       # begin
       #   raise TriangleError

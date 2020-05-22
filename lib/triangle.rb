db04a636c019b2ca1f1c5a9178f5558260714896
claudia v <claudia4ro@yahoo.com>
2020-05-22 01:21:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,19 +12,10 @@ class Triangle
   def kind
     if length_1 <= 0 || length_2 <= 0 || length_3 <= 0
       raise TriangleError
-      # begin
-      #   raise TriangleError
-      # rescue TriangleError => error 
-      #     puts error.message 
-      # end
+      
       
     elsif (length_1 + length_2) <= length_3 || (length_2 + length_3) <= length_1 || (length_1 + length_3) <= length_2
       raise TriangleError
-      # begin
-      #   raise TriangleError
-      # rescue TriangleError => error 
-      #     puts error.message 
-      # end
       
     else
       if length_1 == length_2 && length_1 == length_3 
